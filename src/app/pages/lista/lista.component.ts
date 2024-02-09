47febd4b4fbdeb0202140287e09dc9f822ea94fd
guleao <guztavusz@gmail.com>
2024-02-09 11:47:25 -0300

    Atualização de lista

--- src/app/pages/lista/lista.component.ts
+++ src/app/pages/lista/lista.component.ts
@@ -2,9 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NavbarlistaComponent } from '../../layout/navbars/navbarlista/navbarlista.component';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { Produto } from '../../models/produto';
-import { ProdutoService } from '../../services/produto.service';
 import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
+import { ProdutoService } from '../../services/produto.service';
 
 @Component({
   selector: 'app-lista',
@@ -20,5 +19,51 @@ export class ListaComponent implements OnInit {
 
   async ngOnInit() {
     this.lista = await this.produtoService.getLista();
+  }
+
+  Nome_produto = '';
+  Descricao = '';
+  Preco_compra = 0;
+  Preco_venda = 0;
+
+  OpenModalEditar() {
+    const modelDiv = document.getElementById('modalEditar');
+    if (modelDiv != null) {
+    }
+  }
+
+  CloseModalEditar() {
+    const modelDiv = document.getElementById('modalEditar');
+    if (modelDiv != null) {
+      modelDiv.style.display = 'none';
+    }
+  }
+
+  OpenModalNovo() {
+    const modelDiv = document.getElementById('modalNovo');
+    if (modelDiv != null) {
+      modelDiv.style.display = 'none';
+    }
+  }
+
+  CloseModalNovo() {
+    const modelDiv = document.getElementById('modalNovo');
+    if (modelDiv != null) {
+      modelDiv.style.display = 'none';
+    }
+  }
+
+  OpenModalExcluir() {
+    const modelDiv = document.getElementById('modalExcluir');
+    if (modelDiv != null) {
+      modelDiv.style.display = 'block';
+    }
+  }
+
+  CloseModalExcluir() {
+    const modelDiv = document.getElementById('modalExcluir');
+    if (modelDiv != null) {
+      modelDiv.style.display = 'none';
+    }
   }
 }
--- src/app/pages/menuestoque/menuestoque.component.ts
+++ src/app/pages/menuestoque/menuestoque.component.ts
@@ -12,10 +12,8 @@ import { NavbarvendasComponent } from '../../layout/navbars/navbarvendas/navbarv
 import { Produto } from '../../models/produto';
 import { FormsModule } from '@angular/forms';
 import { ListaComponent } from '../lista/lista.component';
-import { ProdutoService } from '../../services/produto.service';
 import { CommonModule } from '@angular/common';
 import * as bootstrap from 'bootstrap';
-import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-menuestoque',
@@ -32,8 +30,6 @@ import { Modal } from 'bootstrap';
   styleUrl: './menuestoque.component.css',
 })
 export class MenuestoqueComponent {
-  idInserido: number | null = null;
-  produtoParaEditar: Produto | null | undefined = null;
   modalEditarInstance: any;
 
   @ViewChild('modalEditar') modalEditar!: ElementRef;
@@ -42,41 +38,6 @@ export class MenuestoqueComponent {
     this.modalEditarInstance = new bootstrap.Modal(
       this.modalEditar.nativeElement
     );
-  }
-
-  OpenModalEditar() {
-    if (this.idInserido !== null) {
-      this.produtoParaEditar = this.ProdutoService.getProdutoId(
-        this.idInserido
-      );
-      if (this.produtoParaEditar) {
-        this.modalEditarInstance.show();
-      } else {
-        console.log('Erro: Produto não encontrado');
-      }
-    }
-  }
-
-  CloseModalEditar() {
-    const modelDiv = document.getElementById('modalEditar');
-    if (modelDiv != null) {
-      modelDiv.style.display = 'none';
-    }
-    this.CloseModalColocarId();
-  }
-
-  OpenModalNovo() {
-    const modelDiv = document.getElementById('modalNovo');
-    if (modelDiv != null) {
-      modelDiv.style.display = 'block';
-    }
-  }
-
-  CloseModalNovo() {
-    const modelDiv = document.getElementById('modalNovo');
-    if (modelDiv != null) {
-      modelDiv.style.display = 'none';
-    }
   }
 
   OpenModalPerca() {
@@ -92,70 +53,4 @@ export class MenuestoqueComponent {
       modelDiv.style.display = 'none';
     }
   }
-
-  OpenModalExcluir() {
-    const modelDiv = document.getElementById('modalExcluir');
-    if (modelDiv != null) {
-      modelDiv.style.display = 'block';
-    }
-  }
-
-  CloseModalExcluir() {
-    const modelDiv = document.getElementById('modalExcluir');
-    if (modelDiv != null) {
-      modelDiv.style.display = 'none';
-    }
-  }
-
-  OpenModalColocarId() {
-    const modelDiv = document.getElementById('modalColocarId');
-    if (modelDiv != null) {
-      modelDiv.style.display = 'block';
-    }
-  }
-
-  CloseModalColocarId() {
-    const modelDiv = document.getElementById('modalColocarId');
-    if (modelDiv != null) {
-      modelDiv.style.display = 'none';
-    }
-  }
-
-  /* INICIO DO CÓDIGO PARA AS MODALS FUNCIONAREM */
-
-  Nome_produto = '';
-  Descricao = '';
-  Preco_compra = 0;
-  Preco_venda = 0;
-
-  constructor(private ProdutoService: ProdutoService) {
-    this.produtoParaEditar = new Produto(0, '', '', 0, 0);
-  }
-
-  adicionarProdutoALista() {
-    const id = this.ProdutoService.getProximoId();
-    if (this.Nome_produto.trim() !== '') {
-      let novoProduto = new Produto(
-        id,
-        this.Nome_produto,
-        this.Descricao,
-        this.Preco_compra,
-        this.Preco_venda
-      );
-
-      this.ProdutoService.adicionarProduto(novoProduto);
-
-      this.Nome_produto = '';
-      this.Descricao = '';
-      this.Preco_compra = 0;
-      this.Preco_venda = 0;
-    }
-
-    const modelDiv = document.getElementById('modalNovo');
-    if (modelDiv != null) {
-      modelDiv.style.display = 'none';
-    }
-  }
-
-  /* Atualizar produto */
 }
