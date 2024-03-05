d41abf074f4a8b9d1b6b06d5c0790006c9f2fc92
guleao <guztavusz@gmail.com>
2024-03-05 17:04:30 -0300

    Ajuste tela de vendas

--- src/app/pages/pedido/pedido.component.ts
+++ src/app/pages/pedido/pedido.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
 import { NavbarmesasComponent } from '../../layout/navbars/navbarmesas/navbarmesas.component';
+import { CommonModule } from '@angular/common';
+import { Produto } from '../../models/produto';
+import { ProdutoService } from '../../services/produto.service';
 
 @Component({
   selector: 'app-pedido',
   standalone: true,
-  imports: [NavbarmesasComponent],
+  imports: [NavbarmesasComponent, CommonModule],
   templateUrl: './pedido.component.html',
   styleUrl: './pedido.component.css',
 })
-export class PedidoComponent {}
+export class PedidoComponent {
+  lista: Produto[] = [];
+
+  constructor(private produtoService: ProdutoService) {}
+
+  async ngOnInit() {
+    this.lista = await this.produtoService.getLista();
+  }
+
+  filtrar(): void {
+    let ul: HTMLUListElement,
+      li: HTMLLIElement,
+      a: HTMLAnchorElement,
+      i: number,
+      input: HTMLInputElement,
+      filter: string,
+      txtValue: string,
+      count: number = 0;
+
+    // Pegando elementos
+    input = document.getElementById('barraPesquisa') as HTMLInputElement;
+    ul = document.getElementById('listaProdutos') as HTMLUListElement;
+
+    //Filtro
+    filter = input.value.toUpperCase();
+
+    //Pegar as opções da lista
+  }
+}
